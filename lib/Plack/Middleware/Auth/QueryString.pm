c313df6ca473ccb8b07c112adb29ff6ef385632b
Masayuki Matsuki <y.songmu@gmail.com>
2012-05-24 02:04:09 +0900

    implemented

--- lib/Plack/Middleware/Auth/QueryString.pm
+++ lib/Plack/Middleware/Auth/QueryString.pm
@@ -3,6 +3,45 @@ use strict;
 use warnings;
 our $VERSION = '0.01';
 
+use parent qw/Plack::Middleware/;
+
+use Plack::Util::Accessor qw/key password/;
+use Plack::Request;
+
+sub prepare_app {
+    my $self = shift;
+
+    $self->key('key') unless $self->key;
+    die 'requires password' unless $self->password;
+}
+
+sub call {
+    my ($self, $env) = @_;
+
+    return $self->validate($env) ? $self->app->($env) : $self->unauthorized;
+}
+
+sub validate {
+    my ($self, $env) = @_;
+
+    my $req = Plack::Request->new($env);
+    $req->query_parameters->get($self->key) eq $self->password;
+}
+
+sub unauthorized {
+    my $self = shift;
+
+    my $body = 'Authorization required';
+    return [
+        401,
+        [
+            'Content-Type'    => 'text/plain',
+            'Content-Lentgth' => length $body,
+        ],
+        [$body],
+    ];
+}
+
 1;
 __END__
 
